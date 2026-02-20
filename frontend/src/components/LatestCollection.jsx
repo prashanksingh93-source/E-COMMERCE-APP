5f3bd230f917b32090c51f66c86d97fef5949d00
prashanksingh93-source <prashanksingh93@gmail.com>
2026-02-20 16:20:32 +0530

    done latest collection

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import { useContext } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { ShopContext } from '../context/ShopContext';
 import Titel from './Titel';
+import ProductItem from './ProductItem';
 
 const LatestCollection = () => {
 
-    const {products}=useContext(ShopContext);
-    console.log(products)
-  return (
-    <div className='my-10'>
-        <div className='text-center py-8 text-3xl'>
-            <Titel text1={'LATEST'} text2={'COLLECTIONs'}/>
+    const { products } = useContext(ShopContext);
+    const [latestProduct, setLatestProduct] = useState([]);
+
+    useEffect(() => {
+        setLatestProduct(products.slice(0, 10));
+    }, [products]);
+
+    return (
+        <div className='my-10'>
+            <div className='text-center py-8 text-3xl'>
+                <Titel text1={'LATEST'} text2={'COLLECTIONS'} />
+                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, veritatis!
+                </p>
+            </div>
+
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+                {latestProduct.map((item) => (
+                    <ProductItem
+                        key={item._id}
+                        id={item._id}
+                        image={item.image}
+                        name={item.name}
+                        price={item.price}
+                    />
+                ))}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default LatestCollection
