5dd0958295960b6979b30325448224f7a599e57a
prashanksingh93-source <prashanksingh93@gmail.com>
2026-02-21 14:56:07 +0530

    home page complete

---
+++ frontend/src/components/NewsletterBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NewsletterBox = () => {
+
+    const onSubmitHandler=(event)=>{
+        event.preventDefault();
+    }
+
+  return (
+    <div className='text-center'>
+     <p className='text-2xl font-medium text-gray-800'>use refrel & get 10% off</p>
+     <p className='text-gray-400 mt-3'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, reiciendis.</p>
+
+     <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex item-center gap-3 mx-auto my-6 border pl-3' action="">
+        <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' />
+        <button type='submit' className='bg-black text-white text-xs px-10 py-4'>submit</button>
+     </form>
+    </div>
+  )
+}
+
+export default NewsletterBox
